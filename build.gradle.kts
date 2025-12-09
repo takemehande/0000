6c91d23a05b57c5e2e455cbf26e76d11935d7a8a
takemehande <150172968+takemehande@users.noreply.github.com>
2025-12-10 05:28:24 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,108 +14,154 @@
  * You should have received a copy of the GNU General Public License
  * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2021 - 2022 LSPosed Contributors
+ * Copyright (C) 2021 LSPosed Contributors
  */
 
-import com.android.build.api.dsl.ApplicationDefaultConfig
-import com.android.build.api.dsl.CommonExtension
-import com.android.build.gradle.api.AndroidBasePlugin
+import java.time.Instant
 
 plugins {
-    alias(libs.plugins.lsplugin.cmaker)
-    alias(libs.plugins.lsplugin.jgit)
-    alias(libs.plugins.agp.lib) apply false
-    alias(libs.plugins.agp.app) apply false
-    alias(libs.plugins.nav.safeargs) apply false
+    alias(libs.plugins.agp.app)
+    alias(libs.plugins.nav.safeargs)
+    alias(libs.plugins.autoresconfig)
+    alias(libs.plugins.materialthemebuilder)
+    alias(libs.plugins.lsplugin.resopt)
+    alias(libs.plugins.lsplugin.apksign)
 }
 
-cmaker {
-    default {
-        arguments.addAll(
-            arrayOf(
-                "-DEXTERNAL_ROOT=${File(rootDir.absolutePath, "external")}",
-                "-DCORE_ROOT=${File(rootDir.absolutePath, "core/src/main/jni")}",
-                "-DANDROID_STL=none"
-            )
-        )
-        val flags = arrayOf(
-            "-DINJECTED_AID=$injectedPackageUid",
-            "-Wno-gnu-string-literal-operator-template",
-            "-Wno-c++2b-extensions",
-        )
-        cFlags.addAll(flags)
-        cppFlags.addAll(flags)
-        abiFilters("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+apksign {
+    storeFileProperty = "androidStoreFile"
+    storePasswordProperty = "androidStorePassword"
+    keyAliasProperty = "androidKeyAlias"
+    keyPasswordProperty = "androidKeyPassword"
+}
+
+val defaultManagerPackageName: String by rootProject.extra
+
+android {
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
+
+    defaultConfig {
+        applicationId = defaultManagerPackageName
+        buildConfigField("long", "BUILD_TIME", Instant.now().epochSecond.toString())
+    }
+
+    packaging {
+        resources {
+            excludes += "META-INF/**"
+            excludes += "okhttp3/**"
+            excludes += "kotlin/**"
+            excludes += "org/**"
+            excludes += "**.properties"
+            excludes += "**.bin"
+        }
+    }
+
+    dependenciesInfo.includeInApk = false
+
     buildTypes {
-        if (it.name == "release") {
-            arguments += "-DDEBUG_SYMBOLS_PATH=${
-                layout.buildDirectory.dir("symbols").get().asFile.absolutePath
-            }"
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles("proguard-rules.pro")
         }
     }
+
+    sourceSets {
+        named("main") {
+            res {
+                srcDirs("src/common/res")
+            }
+        }
+    }
+    namespace = defaultManagerPackageName
 }
 
-val repo = jgit.repo()
-val commitCount = (repo?.commitCount("refs/remotes/origin/dev") ?: 1) + 4200
-val latestTag = repo?.latestTag?.removePrefix("v")?.substringBefore("-") ?: "1.9.2"
-
-val injectedPackageName by extra("com.android.shell")
-val injectedPackageUid by extra(2000)
-
-val defaultManagerPackageName by extra("org.lsposed.manager")
-val verCode by extra(commitCount)
-val verName by extra(latestTag)
-val androidTargetSdkVersion by extra(36)
-val androidMinSdkVersion by extra(27)
-val androidBuildToolsVersion by extra("36.0.0")
-val androidCompileSdkVersion by extra(36)
-val androidCompileNdkVersion by extra(libs.versions.ndk.get())
-val androidSourceCompatibility by extra(JavaVersion.VERSION_21)
-val androidTargetCompatibility by extra(JavaVersion.VERSION_21)
-val androidCmakeVersion by extra("3.28.0+")
-
-tasks.register("Delete", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+autoResConfig {
+    generateClass = true
+    generateRes = false
+    generatedClassFullName = "org.lsposed.manager.util.LangList"
+    generatedArrayFirstItem = "SYSTEM"
 }
 
-subprojects {
-    plugins.withType(AndroidBasePlugin::class.java) {
-        extensions.configure(CommonExtension::class.java) {
-            compileSdk = androidCompileSdkVersion
-            ndkVersion = androidCompileNdkVersion
-            buildToolsVersion = androidBuildToolsVersion
-
-            externalNativeBuild {
-                cmake {
-                    version = androidCmakeVersion
-                }
-            }
-
-            defaultConfig {
-                minSdk = androidMinSdkVersion
-                if (this is ApplicationDefaultConfig) {
-                    targetSdk = androidTargetSdkVersion
-                    versionCode = verCode
-                    versionName = verName
-                }
-            }
-
-            lint {
-                abortOnError = true
-                checkReleaseBuilds = false
-            }
-
-            compileOptions {
-                sourceCompatibility = androidSourceCompatibility
-                targetCompatibility = androidTargetCompatibility
+materialThemeBuilder {
+    themes {
+        for ((name, color) in listOf(
+            "Red" to "F44336",
+            "Pink" to "E91E63",
+            "Purple" to "9C27B0",
+            "DeepPurple" to "673AB7",
+            "Indigo" to "3F51B5",
+            "Blue" to "2196F3",
+            "LightBlue" to "03A9F4",
+            "Cyan" to "00BCD4",
+            "Teal" to "009688",
+            "Green" to "4FAF50",
+            "LightGreen" to "8BC3A4",
+            "Lime" to "CDDC39",
+            "Yellow" to "FFEB3B",
+            "Amber" to "FFC107",
+            "Orange" to "FF9800",
+            "DeepOrange" to "FF5722",
+            "Brown" to "795548",
+            "BlueGrey" to "607D8F",
+            "Sakura" to "FF9CA8"
+        )) {
+            create("Material$name") {
+                lightThemeFormat = "ThemeOverlay.Light.%s"
+                darkThemeFormat = "ThemeOverlay.Dark.%s"
+                primaryColor = "#$color"
             }
         }
     }
-    plugins.withType(JavaPlugin::class.java) {
-        extensions.configure(JavaPluginExtension::class.java) {
-            sourceCompatibility = androidSourceCompatibility
-            targetCompatibility = androidTargetCompatibility
-        }
-    }
+    // Add Material Design 3 color tokens (such as palettePrimary100) in generated theme
+    // rikka.material >= 2.0.0 provides such attributes
+    generatePalette = true
+}
+
+dependencies {
+    annotationProcessor(libs.glide.compiler)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.startup.runtime)
+    implementation(libs.glide)
+    implementation(libs.material)
+    implementation(libs.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.dnsoverhttps)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.rikkax.appcompat)
+    implementation(libs.rikkax.core)
+    implementation(libs.rikkax.insets)
+    implementation(libs.rikkax.material)
+    implementation(libs.rikkax.material.preference)
+    implementation(libs.rikkax.recyclerview)
+    implementation(libs.rikkax.widget.borderview)
+    implementation(libs.rikkax.widget.mainswitchbar)
+    implementation(libs.rikkax.layoutinflater)
+    implementation(libs.appiconloader)
+    implementation(libs.hiddenapibypass)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(projects.services.managerService)
+
+    debugImplementation(libs.appcenter.analytics)
+    debugImplementation(libs.appcenter.crashes)
+}
+
+configurations.all {
+    exclude("org.jetbrains", "annotations")
+    exclude("androidx.appcompat", "appcompat")
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk7")
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
 }
